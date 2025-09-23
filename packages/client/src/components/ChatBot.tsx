9810cb9fce93bd3d9b5b3b2ce21f1d327be63b37
Tahir Ahmed Syed <tahirahmedsyed3@gmail.com>
2025-09-23 12:32:23 +0300

    copy message code refracted

--- packages/client/src/components/ChatBot.tsx
+++ packages/client/src/components/ChatBot.tsx
@@ -19,30 +19,29 @@ type Message = {
 }
 
 const ChatBot = () => {
- const [messages, setMessages] = useState<Message[]>([])
- const [isBotTyping, setIsBotTyping] = useState(false)
- const formRef = useRef<HTMLFormElement | null>(null)
+  const [messages, setMessages] = useState<Message[]>([])
+  const [isBotTyping, setIsBotTyping] = useState(false)
+  const formRef = useRef<HTMLFormElement | null>(null)
   const conversationId = useRef(crypto.randomUUID())
   const { register, handleSubmit, reset, formState } = useForm<FormData>({
     mode: "onChange"
   });
 
   useEffect(() => {
-    formRef.current?.scrollIntoView({behavior: 'smooth'})
+    formRef.current?.scrollIntoView({ behavior: 'smooth' })
   }, [messages])
 
-  const onSubmit = async ({prompt}: FormData) => {
-
-    setMessages((prev) => [...prev,  {content: prompt, role: 'user'}]);
+  const onSubmit = async ({ prompt }: FormData) => {
+    setMessages((prev) => [...prev, { content: prompt, role: 'user' }]);
     setIsBotTyping(true)
 
     reset();
-    const {data} = await axios.post<ChatResponse>('/api/chat', {
+    const { data } = await axios.post<ChatResponse>('/api/chat', {
       prompt,
       conversationId: conversationId.current
     })
 
-    setMessages(prev => [...prev, {content: data.message, role: 'bot'}])
+    setMessages(prev => [...prev, { content: data.message, role: 'bot' }])
     setIsBotTyping(false)
     console.log(data)
   };
@@ -54,46 +53,60 @@ const ChatBot = () => {
     }
   };
 
+  const onCopyMessage = (e: React.ClipboardEvent) => {
+    const selection = window.getSelection()?.toString().trim()
+    if (selection) {
+      e.preventDefault()
+      e.clipboardData.setData('text/plain', selection);
+    }
+  };
+
   return (
-   <div>
-    <div className='flex flex-col gap-3 mb-10 m-4'>
-      {messages.map((message, index) => <p className={`px-3 py-1 rounded-xl ${
-        message.role === 'user' ?
-         'bg-blue-600 text-white self-end' : 'bg-gray-100 text-black self-start' 
-      }`} key={index}>
-        <ReactMarkdown>
-        {message.content}
-        </ReactMarkdown>     
-        </p>)}
+    <div>
+      <div className='flex flex-col gap-3 mb-10 m-4'>
+        {messages.map((message, index) =>
+          <p
+            key={index}
+            onCopy={onCopyMessage}
+            className={`px-3 py-1 rounded-xl ${
+              message.role === 'user'
+                ? 'bg-blue-600 text-white self-end'
+                : 'bg-gray-100 text-black self-start'
+            }`}
+          >
+            <ReactMarkdown>
+              {message.content}
+            </ReactMarkdown>
+          </p>)}
         {isBotTyping && (
-        <div className='flex self-start gap-1 px-3 py-3 bg-gray-200 rounded-xl'>
-          <div className='w-2 h-2 rounded-full bg-gray-800 animate-pulse'></div>
-          <div className='w-2 h-2 rounded-full bg-gray-800 animate-pulse [animation-delay:0.2s]'></div>
-          <div className='w-2 h-2 rounded-full bg-gray-800 animate-pulse [animation-delay:0.4s]'></div>
-        </div>
+          <div className='flex self-start gap-1 px-3 py-3 bg-gray-200 rounded-xl'>
+            <div className='w-2 h-2 rounded-full bg-gray-800 animate-pulse'></div>
+            <div className='w-2 h-2 rounded-full bg-gray-800 animate-pulse [animation-delay:0.2s]'></div>
+            <div className='w-2 h-2 rounded-full bg-gray-800 animate-pulse [animation-delay:0.4s]'></div>
+          </div>
         )}
-    </div>
-     <form
-      onSubmit={handleSubmit(onSubmit)}
-      onKeyDown={onKeyDown}
-      ref={formRef}
-      className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl m-4"
-    >
-      <textarea
-        {...register('prompt', { required: true, validate: (data) => data.trim().length > 0 })}
-        className="w-full border-0 focus:outline-0 resize-none"
-        placeholder="ask anything"
-        maxLength={1000}
-      />
-      <Button
-        type="submit"
-        disabled={!formState.isValid}
-        className="rounded-full w-9 h-9 flex items-center justify-center"
+      </div>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        onKeyDown={onKeyDown}
+        ref={formRef}
+        className="flex flex-col gap-2 items-end border-2 p-4 rounded-3xl m-4"
       >
-        <FaArrowUp />
-      </Button>
-    </form>
-   </div>
+        <textarea
+          {...register('prompt', { required: true, validate: (data) => data.trim().length > 0 })}
+          className="w-full border-0 focus:outline-0 resize-none"
+          placeholder="ask anything"
+          maxLength={1000}
+        />
+        <Button
+          type="submit"
+          disabled={!formState.isValid}
+          className="rounded-full w-9 h-9 flex items-center justify-center"
+        >
+          <FaArrowUp />
+        </Button>
+      </form>
+    </div>
   );
 };
 
